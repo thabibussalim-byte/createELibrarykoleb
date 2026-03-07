d9b3d08701ccf51c66252c69e064eb907a112cef
Chairil Rahman <chairilrahman130@gmail.com>
2026-03-07 22:11:02 +0700

    menambahkan kode

--- app/src/main/java/com/example/elibrarypetik/ui/mainview/MainViewModel.kt
+++ app/src/main/java/com/example/elibrarypetik/ui/mainview/MainViewModel.kt
@@ -1,4 +1,16 @@
-package com.example.elibrarypetik.ui.mainView
+package com.example.elibrarypetik.ui.mainview
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _errorMessage = MutableLiveData<String?>()
+    val errorMessage: LiveData<String?> = _errorMessage
 
 class MainViewModel {
 }
