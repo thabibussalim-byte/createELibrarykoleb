205f418197c5c1ca7dcdd0d3261e9e7eda3395c1
Chairil Rahman <chairilrahman130@gmail.com>
2026-04-06 20:31:42 +0700

    Menambahkan 7 clicklistener agar menampilkan informasi hal faq

--- app/src/main/java/com/example/elibrarypetik/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/elibrarypetik/ui/home/HomeFragment.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.elibrarypetik.R
 import com.example.elibrarypetik.data.api.ApiConfig
 import com.example.elibrarypetik.data.api.model.GenreResponse
 import com.example.elibrarypetik.data.model.Book
